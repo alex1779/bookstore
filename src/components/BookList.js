bf728902b233bc048c6a0af6bf1c72a0944aee32
alex1779 <alex1779@gmail.com>
2023-09-27 19:26:26 -0300

    Created new components for form and book list

---
+++ src/components/BookList.js
@@ -0,0 +1,23 @@
+function BookList() {
+  return (
+    <ul>
+
+      <li>
+        <h4>Action</h4>
+        <h2>The Hunger Games</h2>
+        <h6>Suzanne Collins</h6>
+        <button type="button">Remove</button>
+      </li>
+
+      <li>
+        <h4>Science Fiction</h4>
+        <h2>Dune</h2>
+        <h6>Frank Herbert</h6>
+        <button type="button">Remove</button>
+      </li>
+
+    </ul>
+  );
+}
+
+export default BookList;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,14 @@
+import Newbookform from './Newbookform';
+import BookList from './BookList';
+
 function Home() {
   return (
     <div>
       <div>
-
-        <ul>
-
-          <li>
-            <h4>Action</h4>
-            <h2>The Hunger Games</h2>
-            <h6>Suzanne Collins</h6>
-            <button type="button">Remove</button>
-          </li>
-
-          <li>
-            <h4>Science Fiction</h4>
-            <h2>Dune</h2>
-            <h6>Frank Herbert</h6>
-            <button type="button">Remove</button>
-          </li>
-
-        </ul>
-
+        <BookList />
         <div />
-
-        <section>
-          <h2>ADD NEW BOOK</h2>
-          <form>
-            <input placeholder="Book title" required="" />
-            <input placeholder="Author" required="" />
-            <button type="submit">ADD BOOK</button>
-          </form>
-        </section>
-
+        <Newbookform />
       </div>
-
     </div>
   );
 }
