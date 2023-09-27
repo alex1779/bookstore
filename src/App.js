a69248f628efaeb18336ff7afbadf3d91f9ed80a
alex1779 <alex1779@gmail.com>
2023-09-27 19:16:49 -0300

    set up router on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import RouterNav from './components/Router';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import ErrorPage from './components/Errorpage';
 
 function App() {
   return (
-    <RouterNav />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/categories" element={<Categories />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
