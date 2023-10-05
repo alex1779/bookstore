d2d77ac7c9eb67d5c63dfd4c01210a702e6c68fc
alex1779 <alex1779@gmail.com>
2023-10-04 21:39:26 -0300

    Aplly useEffect and change bookdata

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Book from './Book';
+import { fetchBooksFromApi } from '../redux/books/booksSlice';
 
 function BookList() {
+  const dispatch = useDispatch();
   const { bookList } = useSelector((store) => store.books);
+
+  useEffect(() => {
+    dispatch(fetchBooksFromApi());
+  }, []);
+
   return (
     <ul>
       {bookList.map((item) => (
-        <li key={item.id}>
+        <li key={item.item_id}>
           <Book
             bookData={item}
           />
